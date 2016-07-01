888169c2d769e870daf734290ce109c7c2148c09
Eric Stern <eric@ericstern.com>
2016-07-01 00:54:26 -0700

    Add installer

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+files=(.vim .vimrc .bash_profile .screenrc)
+
+# Trick to get absolute path to this script
+# http://stackoverflow.com/a/4774063
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+
+# Update submodules (e.g. vim plugins) while in known directory
+git submodule init
+git submodule update
+
+popd > /dev/null
+
+echo "Run these to symlink the dotfiles"
+for file in ${files[@]}; do
+    echo ln -s $SCRIPTPATH/$file $HOME/$file
+done
