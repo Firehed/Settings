cfef44750382ca21b4e7b0d4cc6fbe8b96839e6d
Eric Stern <eric@ericstern.com>
2017-02-15 11:10:27 -0800

    Add global gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=(.vim .bash_profile .bashrc .screenrc .inputrc .ackrc)
+files=(.vim .bash_profile .bashrc .screenrc .inputrc .ackrc .gitignore_global)
 
 # Trick to get absolute path to this script
 # http://stackoverflow.com/a/4774063
